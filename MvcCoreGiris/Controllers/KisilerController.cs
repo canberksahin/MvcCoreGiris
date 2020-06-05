730c8fcdf68288724a53dae3616085e49ecc721e
Canberk Sahin <canberksahin20@hotmail.com>
2020-06-05 22:12:42 +0300

    Add project files.

---
+++ MvcCoreGiris/Controllers/KisilerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MvcCoreGiris.Models;
+
+namespace MvcCoreGiris.Controllers
+{
+    public class KisilerController : Controller
+    {
+        private readonly OkulCoreContext db;
+        public KisilerController(OkulCoreContext okulcoreContext)
+        {
+            db = okulcoreContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View(db.Kisiler.ToList());
+        }
+    }
+}
---
+++ MvcCoreGiris/Models/OkulCoreContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreGiris.Models
+{
+    public class OkulCoreContext : DbContext
+    {
+        public OkulCoreContext(DbContextOptions<OkulCoreContext> options) : base(options)
+        {
+
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("server=.;database=OkulCoreContext;uid=sa;pwd=123;");
+        //}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Kisi>().HasData(
+                new Kisi { Id=1, KisiAd="Can"},
+                new Kisi { Id=2, KisiAd="Canan"}
+                );
+
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Kisi> Kisiler { get; set; }
+    }
+}
